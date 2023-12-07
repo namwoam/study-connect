65c799680d401fce6c0a9a2387d0f9ea27220a48
GaryHuang1122 <garyhuang@ntu.im>
2023-12-08 00:27:27 +0800

    login css

--- study-connect-frontend/src/containers/Login.js
+++ study-connect-frontend/src/containers/Login.js
@@ -25,49 +25,70 @@ const Login = ({setLogin, setuser}) => {
   };
 
   return (
-    <Container component="main" maxWidth="xs">
-      <Paper elevation={3} style={{ padding: 20, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <Typography component="h1" variant="h5">
-          Login
-        </Typography>
-        <form style={{ width: '100%', marginTop: 10 }}>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="username"
-            label="Username"
-            name="username"
-            autoComplete="username"
-            autoFocus
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Button
-            type="button"
-            fullWidth
-            variant="contained"
-            color="primary"
-            onClick={handleLogin}
-            style={{ marginTop: 20 }}
+    <Container component="main" maxWidth="xl" style={{ height: '85vh', display: 'flex', flexDirection: 'column' }}>
+      <Paper elevation={3} style={{ height: '100vh', padding: 20, display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+      <Grid container style={{ height: '80vh'}}>
+        <Grid 
+            item
+            xs={6}
+            style={{
+              background: 'linear-gradient(to bottom, #ffdcB9, #ff5733)',
+              borderRadius: '20px',
+              padding: 20,
+              height: '100%',
+            }}
           >
-            Login
-          </Button>
-        </form>
+        </Grid>
+        <Grid item xs={6} style={{ padding: 20, display: 'flex', flexDirection: 'column',justifyContent: 'center', alignItems: 'center' }}>
+          <Typography component="h3" variant="h5" style={{ position: 'absolute', top: 10, right: 20, color: '#ff8000'}}>
+            StudyConnect
+          </Typography>
+          <Typography component="h8" variant="h8">
+            Welcome to StudyConnect!
+          </Typography>
+          <Typography component="h1" variant="h3">
+            Sign in 
+          </Typography>
+          <form style={{ width: '100%', marginTop: 10, justifyContent: 'right' }}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              name="username"
+              autoComplete="username"
+              autoFocus
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+              type="button"
+              fullWidth
+              variant="contained"
+              color="primary"
+              onClick={handleLogin}
+              style={{ marginTop: 20, width: '50%' }}
+            >
+              Login
+            </Button>
+          </form>
+          </Grid>
+        </Grid>
       </Paper>
       <Snackbar
         anchorOrigin={{
