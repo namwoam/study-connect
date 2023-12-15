368fddc4a35d3436fa5cba96d9af824f4a735972
Cody <c0dyp3n6@gmail.com>
2023-12-15 18:31:08 +0800

    Add CreateGroupModal.js in frontend/src/components

--- study-connect-frontend/src/components/CreateGroupModal.js
+++ study-connect-frontend/src/components/CreateGroupModal.js
@@ -7,8 +7,7 @@ import {
     Container,
     Modal,
     TextField,
-    Paper,
-    MenuItem
+    Paper
   } from '@mui/material';
 
 const ModelStyle = {
@@ -30,19 +29,46 @@ const ModelStyle = {
     borderRadius: '30px',
 };
 
-const CreateGroupModal = ({open, setOpen, userCourses}) => {
+// 應該要用userID去取user這學期有修的課程 開一個下拉式選單讓他選
+// 除了那個 其他地方應該都是完成的了
 
+
+const CreateGroupModal = ({open, setOpen, userID}) => {
     const handleClose = () => setOpen(false);
 
-    const [editingLeader, setEditingLeader] = useState("");
+    const [editingGroupName, setEditingGroupName] = useState("");
+    const [editingMaximumMember, setEditingMaximumMember] = useState(2);
+    const [editingCourseID, setEditingCourseID] = useState("");
 
-    const handleLeaderChange = (event) => {
-        setEditingLeader(event.target.value);
+    const handleGroupNameCHange = (event) => {
+        setEditingGroupName(event.target.value);
     };
 
+    const handleMaximumMemberChange = (event) => {
+        setEditingMaximumMember(event.target.value);
+    };
+
+    const handleCourseIDChange = (event) => {
+        setEditingCourseID(event.target.value);
+    }
+
     const handlePublish = () => {
-        console.log('Publish an announcements');
-        console.log(editingLeader);
+        console.log('Publish');
+        try {
+            const response = instance.post('/group/create', {
+                user: userID,
+                group_name: editingGroupName,
+                Capacity: editingMaximumMember,
+                course_id: editingCourseID,
+            });
+            if (response.data.success) {
+                setAlertMessage('Request sent successfully');
+            } else {
+                setAlertMessage('Failed to send request');
+            }
+        } catch (error) {
+            console.log(error);
+        }
         handleClose();
     };
 
@@ -53,42 +79,51 @@ const CreateGroupModal = ({open, setOpen, userCourses}) => {
         >
             <Box sx={ModelStyle}>
                 <Typography variant="h4" align="center" color="primary" sx={{mb: '20px'}}>
-                    創立課程小組
+                    Create group
                 </Typography>
-                {/* set a length = numOfMember scroll down menu to select who will be the group leader */}
                 <TextField
-                    id="outlined-select-currency"
+                    multiline
+                    rows={1}
+                    label="小組名稱"
+                    variant="outlined"
+                    fullWidth
+                    value={editingGroupName}
+                    onChange={handleGroupNameCHange}
+                    sx={{ mt: 2 }}
+                />
+
+                <TextField
                     select
-                    label="Select"
-                    value={editingLeader}
-                    onChange={handleLeaderChange}
-                    helperText="Please select a course to create course group"
+                    label="成員上限"
+                    value={editingMaximumMember}
+                    onChange={handleMaximumMemberChange}
+                    helperText="Please select maximum number of member"
                 >
-                    {userCourses.map((course) => (
-                        <MenuItem key={course.course_id} value={course.course_name}>
-                            {course.course_name}
+                    {[2, 3, 4, 5, 6, 7, 8, 9, 10].map((number) => (
+                        <MenuItem key={number} value={number}>
+                            {number}
                         </MenuItem>
                     ))}
                 </TextField>
-                <Button
-                    id="confirm-update-button"
-                    size="small"
-                    variant="contained"
-                    color="secondary"
-                    onClick={handlePublish}
-                    sx={{
-                        width: '100px',
-                        height: '50px',
-                        borderRadius: '30px',
-                        mt: '25px',
-                        textTransform: 'none',
-                        color: '#fff',
-                        fontSize: '14px',
-                        fontWeight: 600,
-                    }}
-                >
-                    創建小組
-                </Button>
+                    <Button
+                        id="confirm-create-group-button"
+                        size="small"
+                        variant="contained"
+                        color="secondary"
+                        onClick={handlePublish}
+                        sx={{
+                            width: '100px',
+                            height: '50px',
+                            borderRadius: '30px',
+                            mt: '25px',
+                            textTransform: 'none',
+                            color: '#fff',
+                            fontSize: '14px',
+                            fontWeight: 600,
+                        }}
+                    >
+                        創立小組
+                    </Button>
             </Box>
         </Modal>
     );
