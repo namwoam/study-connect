0d8b099db55901340d00ad476501646aa1a8255a
Alison <75669495+Cyalisonliu@users.noreply.github.com>
2023-12-16 21:11:59 +0800

    feat: add admin style

--- study-connect-frontend/src/components/Header.js
+++ study-connect-frontend/src/components/Header.js
@@ -2,7 +2,7 @@ import { React, useState, useEffect } from 'react';
 import { AppBar, Box, Toolbar, Typography, Button, Divider,  Avatar } from '@mui/material';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
-import { deepOrange } from '@mui/material/colors';
+import { deepPurple, deepOrange } from '@mui/material/colors';
 import { fetchUserInfo } from '../utils/fetchUser';
 
 const pages = ['Home', 'Friend', 'Course', 'Group'];
@@ -45,7 +45,9 @@ const Header = ({ userID, currentPage, onPageChange, setIslogin, isAdmin}) => {
 
     return (
         <Box sx={{ flexGrow: 1 }}>
-            <AppBar elevation={1} position="fixed">
+            <AppBar elevation={isAdmin ? 0 : 1} position="fixed" sx={{
+                    backgroundColor: isAdmin && '#424242'
+                }}>
                 <Toolbar>
                     {/* logo */}
                     <Typography variant="h6" fontWeight={800} color='#DE5000'>
@@ -55,13 +57,13 @@ const Header = ({ userID, currentPage, onPageChange, setIslogin, isAdmin}) => {
                     {/* page button */}
                     <Box sx={{ flexGrow: 1, display: 'flex' }}>
                         <Box sx={{ margin: 3}}>
-                            <Divider orientation="vertical" color="gray" />
+                            <Divider orientation="vertical" color={isAdmin ? "white" : "gray"} />
                         </Box>
                         {pages.map((page, index) => (
                             <Button
                                 key={index}
                                 // color="inherit"
-                                sx={{ my: 2, display: 'block', color: currentPage === index ? 'black' : 'gray' }}
+                                sx={{ my: 2, display: 'block', fontWeight: currentPage === index ? 800 : 500, color: currentPage === index ? isAdmin ? 'white' : 'black' : isAdmin ? '#e0e0e0' : 'gray' }}
                                 onClick={() => handlePageClick(index)}
                             >
                                 {page}
@@ -70,27 +72,25 @@ const Header = ({ userID, currentPage, onPageChange, setIslogin, isAdmin}) => {
                     </Box>
 
                     {/* avatar and sign out */}
-                    <Button
-                        sx={{ margin: 1}}
-                        onClick={() => handlePageClick(4)}>
-                        <Avatar size="small" sx={{ height:'24px', width: '24px', marginRight: 1, bgcolor: deepOrange[400] }} />
-                        <Typography variant="subtitle2" color='black'>
+                    <Button sx={{ margin: 1 }} onClick={() => handlePageClick(4)}>
+                        <Avatar size="small" sx={{ height: '24px', width: '24px', marginRight: 1, bgcolor: isAdmin ? deepOrange[400] : deepOrange[400] }} />
+                        <Typography color={isAdmin ? 'white' : 'black'}>
                             {username ?? 'userA'}
                         </Typography>
                     </Button>
-                    {isAdmin ?
-                        <Button 
-                            sx={{ margin: 1}}
-                            color="primary" 
-                            startIcon={<AdminPanelSettingsIcon/>} 
-                            onClick={() => handlePageClick(5)}>
-                                Admin
-                        </Button> : <></>
-                    }
+                    {isAdmin && (
+                        <Button
+                            sx={{ margin: 1, color: 'white' }}
+                            startIcon={<AdminPanelSettingsIcon />}
+                            onClick={() => handlePageClick(5)}
+                        >
+                            Admin
+                        </Button>
+                    )}
                     <Button 
-                        sx={{ margin: 1}}
+                        sx={{ margin: 0, fontWeight: 700}}
                         edge="end" 
-                        size="small" 
+                        // size="small" 
                         color="primary" 
                         endIcon={<ArrowForwardIcon/>} 
                         onClick={handleSignOut}>
